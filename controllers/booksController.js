39fc9983343448292228a415d5443b62c7d8b80d
NazmaCyberlink <nazushakya7@gmail.com>
2024-08-24 11:33:43 +0545

    layouts

---
+++ controllers/booksController.js
@@ -0,0 +1,46 @@
+const PostBook = async (req,res)=>{
+    try{
+
+    }
+    catch(err){
+
+    }
+}
+const GetBookById = async (req,res)=>{
+    try{
+
+    }
+    catch(err){
+        
+    }
+}
+const GetAllBooks = async (req,res)=>{
+    try{
+
+    }
+    catch(err){
+        
+    }
+}
+const UpdateBook = async (req,res)=>{
+    try{
+
+    }
+    catch(err){
+        
+    }
+}
+const DeleteBook = async (req,res)=>{
+    try{
+
+    }
+    catch(err){
+        
+    }
+}
+
+exports.PostBook = PostBook
+exports.GetBookById = GetBookById
+exports.GetAllBooks = GetAllBooks
+exports.UpdateBook =UpdateBook
+exports.DeleteBook = DeleteBook
---
+++ routes/booksRouter.js
@@ -0,0 +1,5 @@
+const { Router }= require('express')
+const app = Router()
+
+
+app.post('/books', )
