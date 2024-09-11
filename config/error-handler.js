4829825bf0bbd371a0249fcf07d492184a221286
Leeza@218 <leezashakya218@gmail.com>
2024-09-11 19:25:58 +0545

    all merged

--- config/error-handler.js
+++ config/error-handler.js
@@ -1,6 +1,5 @@
 export default function errorHandler(err, req, res, next) {
     if(err.name === 'UnauthorizedError') {
-        console.log(err);
         res.status(401).json({message: "User is not authorized"})
     }
 
